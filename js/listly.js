b7427773cc9adb1b665355ac68028e4689efa7e5
lesreaper <rich@tommyland.org>
2015-03-09 16:59:50 -0400

    Remove placeholder LI

--- js/listly.js
+++ js/listly.js
@@ -1,12 +1,25 @@
-function Listly() {
+var Listly =  function() {
   // Listed twice so as to make it a Constructor
   function Listly() {
     this.tasks = [];
 
     var self = this;
-    
+
+    //Look for form descendant new_task, and get when fired a function (event, function call)
+    $('form#new_task').on('submit', function(ev) {
+      ev.preventDefault();
+      // "this" is for the form element, because event handlers always come from a form
+      // Add tasks to list
+      var task_name = this.task_name.value;
+      var result = $('#tasks').append('<li class="list-group-item">'+ task_name +'</li>');
+      this.task_name.value = '';
+      $(this.task_name).focus();
+
+      // Add tasks to local storage
+    });
+
   }
   return Listly;
-}
+}();
 
 var listly = new Listly();
