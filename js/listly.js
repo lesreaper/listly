7a2c1a177d24b80c63ff3fc79631357b91b3c2d7
lesreaper <rich@tommyland.org>
2015-03-09 13:56:36 -0400

    First jQuery update

---
+++ js/listly.js
@@ -0,0 +1,12 @@
+function Listly() {
+  // Listed twice so as to make it a Constructor
+  function Listly() {
+    this.tasks = [];
+
+    var self = this;
+    
+  }
+  return Listly;
+}
+
+var listly = new Listly();
