9c4870679f6da1eb69d5afcb169bd1e0de9fc091
lesreaper <rich@tommyland.org>
2015-03-09 20:48:02 -0400

    Test 2

--- js/listly.js
+++ js/listly.js
@@ -25,7 +25,57 @@ var Listly =  function() {
       } catch(err) {
         return false;
       }
+        return true;var Listly = function() {
+
+  function Listly() {
+    var self = this;
+    self.tasks = [];
+
+    function addTask(task_name) {
+      self.tasks.push(task_name);
+      if (save()) {
+        $('#tasks').append('<li class="list-group-item">' + task_name + '</li>');
         return true;
+      }
+      else {
+        return false;
+      }
+    }
+
+    function load() {
+      self.tasks = JSON.parse(localStorage.tasks);
+      $.each(self.tasks, function(index, task_name) {
+        $('#tasks').append('<li class="list-group-item">' + task_name + '</li>');
+      });
+    }
+
+    function save() {
+      try {
+        return (localStorage.tasks = JSON.stringify(self.tasks));
+      }
+      catch(err) {
+        return false;
+      }
+    }
+
+    load();
+
+    $('form#new_task').on('submit', function(ev) {
+      ev.preventDefault();
+      var field = $(this.task_name);
+      var task_name = field.val();
+
+      if (addTask(task_name)) {
+        field.val('');
+      }
+      field.focus().select();
+    });
+  }
+
+  return Listly;
+}();
+
+var listly = new Listly();
       }
 
     function clearAndFocusField(field) {
